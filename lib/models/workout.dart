2ce0de2c0f84183aac8cde398f08178a4e748285
toma <fko2347021@stu.o-hara.ac.jp>
2025-10-30 13:56:24 +0900

    カレンダー機能、グラフ追加

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -1,13 +1,13 @@
 class Workout {
-  final String name;
-  final String duration;
-  final String imageUrl;
-  final String description;
+  final String name; // 種類（ランニング、筋トレなど）
+  final DateTime date; // 実施日
+  final int duration; // 運動時間（分）
+  final int calories; // 消費カロリー（kcal）
 
   Workout({
     required this.name,
+    required this.date,
     required this.duration,
-    required this.imageUrl,
-    required this.description,
+    required this.calories,
   });
 }
