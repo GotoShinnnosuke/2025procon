623bfb99ab05c9a8daeb14060ecb20a2e19b8481
hikaruaraki <fko2347052@stu.o-hara.ac.jp>
2025-11-10 13:57:36 +0900

    kk

--- lib/services/training_log_firestore.dart
+++ lib/services/training_log_firestore.dart
@@ -1,6 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import '../models/training_menu.dart';
 import 'training_log.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
